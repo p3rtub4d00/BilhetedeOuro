75fdb24302fc58a17ace5c204825dd3a70239c34
p3rtub4d00 <containerbeerpvh@gmail.com>
2026-03-04 12:33:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.locals.db = db; // Disponibiliza o db para as rotas
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
